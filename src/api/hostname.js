d4d157dc730b38eba3536257f8f820d73b4670e6
tuanduong <phanduong2510@gmail.com>
2024-07-10 17:48:57 +0700

    change host

--- src/api/hostname.js
+++ src/api/hostname.js
@@ -1,5 +1,5 @@
 const server = {
-  host_name_deploy: "final-project-backend-pi.vercel.app",
+  host_name_deploy: "project3-production-3d84.up.railway.app:8082",
   host_name_local: "localhost:8081",
 };
 export default server;
